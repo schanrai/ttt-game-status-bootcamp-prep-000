748940404641a2ee0e4b192aab2ded7bd6da0b2e
Sushi Chanrai <sushi@pathworks.co>
2018-06-12 19:18:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,22 @@
 # Helper Method
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+
+#accepts board as argument, 
+#returns false/nil if no win combo present
+#returns the winning combo index as an array if there is a winning -how they won
+
+def won?[board]
+  [WIN_COMBINATIONS].each do |win_combinations|
+  [board].select{|i| i.is_win_combinations?()}
+end
+  
+end
